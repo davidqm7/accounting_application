e86d74ef1ff28b18c914c90b890993714205d0f4
David <davidqm7@outlook.com>
2024-09-20 18:48:44 -0400

    firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,37 @@
+import { getFirestore } from "firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+
+
+import {getAuth} from 'firebase/auth'; 
+
+
+
+const firebaseConfig = {
+
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+
+  authDomain: "swe4713-accounting-application.firebaseapp.com",
+
+  projectId: "swe4713-accounting-application",
+
+  storageBucket: "swe4713-accounting-application.appspot.com",
+
+  messagingSenderId: "49319612085",
+
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+
+  measurementId: "G-M12DWCCL8T"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app); 
+
+const db = getFirestore(app);
+
+
+export {auth ,db}; 
