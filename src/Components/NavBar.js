e162c6d39bc8c685a679cc4bef4e39f6e29211ac
David <davidqm7@outlook.com>
2024-10-08 14:13:00 -0400

    Added Help on every page

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,18 +1,36 @@
-import React from "react";
-import './NavBar.css'; 
-import Logo from './AppLogo.png'
+import React, { useState } from 'react';
+import './NavBar.css';
+import HelpModal from './HelpModal'; // Import HelpModal component
+import Logo from './AppLogo.png';
 
 const NavBar = () => {
+  const [showHelpModal, setShowHelpModal] = useState(false);
+
+  const handleHelpClick = () => {
+    setShowHelpModal(true);  // Show the modal when Help is clicked
+  };
+
+  const handleCloseModal = () => {
+    setShowHelpModal(false);  // Hide the modal when Close is clicked
+  };
+
   return (
     <nav className="NavBar">
       <div className="NavBar-logo">
         <img src={Logo} alt="Team Logo" />
       </div>
-      <p>By David Quintanilla, Jesse Israel, Husain Falih, Jose Mendoza</p>
-      <h1>Numbers Games Accounting</h1> 
+      <p>By David Quintanilla, Jesse Israel, Husain Falih</p>
+      <h1>Numbers Games Accounting</h1>
+      
+      {/* Help Button */}
+      <button className="help-button" onClick={handleHelpClick}>
+        Help
+      </button>
+
+      {/* Help Modal */}
+      <HelpModal show={showHelpModal} handleClose={handleCloseModal} />
     </nav>
   );
-}
-
+};
 
 export default NavBar;
